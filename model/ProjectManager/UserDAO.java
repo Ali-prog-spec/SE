ecfe5568eec1d0d54b3255e214acd03030da87a3
KENKINIKI <m.alenauman@gmail.com>
2026-03-24 11:31:24 +0500

    model

---
+++ model/ProjectManager/UserDAO.java
@@ -0,0 +1,53 @@
+package model.ProjectManager;
+import java.sql.*;
+
+public class UserDAO {
+    
+    public static User authenticateUser(String username, String password) {
+        String query = "SELECT * FROM users WHERE name = ? AND password = ?";
+        
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            
+            ResultSet rs = pstmt.executeQuery();
+            
+            if (rs.next()) {
+                User user = new User();
+                user.setUserId(rs.getInt("id"));
+                user.setUsername(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("role"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static User getUserById(int userId) {
+        String query = "SELECT * FROM users WHERE id= ?";
+        
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            
+            pstmt.setInt(1, userId);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if (rs.next()) {
+                User user = new User();
+                user.setUserId(rs.getInt("id"));
+                user.setUsername(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("role"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
